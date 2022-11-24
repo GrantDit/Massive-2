89e3b1ff85c9863b70a815b02d1f62854829e74f
GrantDit <kiruha5854@mail.ru>
2022-11-24 14:37:51 +0300

    Домашнее задание массивы ч2

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,36 @@ public class Main {
         //Задание 1
         System.out.println("Задание 1");
         int summa = 0;
-        for (int i = 0; i <arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             summa += arr[i];
-            if (i <31);
-            System.out.print("Сумма трат за месяц составила "+ summa +" рублей");
+            if (i < 31) ;
+            System.out.print("Сумма трат за месяц составила " + summa + " рублей");
         }
         //Задание 2
         System.out.println("Задание 2");
         int min = 0;
         int max = 0;
-        for (int i = 0; i <arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             max += arr[i];
-            min += max /arr[i];
-            if (i < 31);
-            System.out.print("Минимальная сумма трат за день составила "+ min+ " рублей. Максимальная сумма трат за день составила "+ max +" рублей».");
+            min += max / arr[i];
+            if (i < 31) ;
+            System.out.print("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей».");
         }
         //Задание 3
         System.out.println();
         System.out.println("Задание 3");
         double total = summa / arr.length;
-        System.out.println("Средняя сумма трат за месяц составила "+ total + " рублей");
+        System.out.println("Средняя сумма трат за месяц составила " + total + " рублей");
 
         //Задание 4
         System.out.println("Задание 4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length-1 ;i >= 0 ; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
 
         }
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
