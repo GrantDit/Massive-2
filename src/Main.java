aa3c8997ece34ed992a145e377b1bd381ba557e9
GrantDit <kiruha5854@mail.ru>
2022-11-25 23:31:59 +0300

    Домашнее задание массивы ч2

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,25 @@ public class Main {
             System.out.print("Сумма трат за месяц составила " + summa + " рублей");
         }
         //Задание 2
+        System.out.println();
         System.out.println("Задание 2");
-        int min = 0;
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            max += arr[i];
-            min += max / arr[i];
-            if (i < 31) ;
-            System.out.print("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей».");
+        int minimum = arr[0];
+        int maximum = arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] > maximum) {
+                maximum = arr[i];
+            }
+            if (arr[i] < minimum) {
+                minimum = arr[i];
+            }
+            System.out.print("Минимальная сумма трат за день составила " + minimum + " рублей. Максимальная сумма трат за день составила " + maximum + " рублей».");
         }
         //Задание 3
         System.out.println();
         System.out.println("Задание 3");
         double total = summa / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + total + " рублей");
+
 
         //Задание 4
         System.out.println("Задание 4");
